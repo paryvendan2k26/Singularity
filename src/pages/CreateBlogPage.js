5b567ff2abb8aa3302edae3378615055ceafb608
alok raj <alokraj1319@gmail.com>
2025-09-27 23:16:00 +0530

    our new BlogListPage has links to /create-blog and /edit-blog/:id. Let's create simple placeholder files for them now so your app doesn't crash when you click the links. We will add the real forms in the next step

---
+++ src/pages/CreateBlogPage.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const CreateBlogPage = () => <div className="text-white text-center py-10">Create Blog Page - Coming Soon!</div>;
+export default CreateBlogPage;
---
+++ src/pages/EditBlogPage.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const EditBlogPage = () => <div className="text-white text-center py-10">Edit Blog Page - Coming Soon!</div>;
+export default EditBlogPage;
