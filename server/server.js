ddb0e038059faf73f9e41dcf2d547cbd7f46038a
alok raj <alokraj1319@gmail.com>
2025-09-27 19:27:52 +0530

    modified the code of server.js in order to connect to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 //backend server // server/server.js
+
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,7 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Allows the server to accept JSON in the body of requests
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully.'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Basic route for testing
 app.get('/', (req, res) => {
